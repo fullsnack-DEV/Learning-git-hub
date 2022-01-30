cb085a04e20a74a85df4a0119d2846039b10bf81
Pratik <pratikpatre2019gmail.com>
2022-01-30 15:17:06 +0530

    Added the files

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 //learning the Git and Git hub. 
 //git status : = >  To check the status of the files 
 //let's modify the file and check the git status
+//git restore : =>  to make it untracked again 
+//git log : => To see all the Logs ( changes that are made)
+//rm -rf (file name) : =>  to delete the file
